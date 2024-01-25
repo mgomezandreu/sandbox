cb69a318eaba91bc5800f1b7ae9fde47f6e101ce
mgomezandreu <mario-gomez-andreu@outlook.com>
2024-01-25 23:12:27 +0100

    loading and deleting model, main simulation loop

--- mujoco_simulation_cpp/src/main.cpp
+++ mujoco_simulation_cpp/src/main.cpp
@@ -10,6 +10,18 @@ int main(){
 	char* errstr;
 	int errstr_sz;
 	mjModel* m = mj_loadXML("../models/inverted_pendulum.xml", NULL, errstr, errstr_sz);
+	mjData* d = mj_makeData(m);
+
+
+	while(d->time <10){
+		mj_step(m,d);
+		std::cout << "Stepping " << d->time << std::endl;
+	}
+
+
+	mj_deleteModel(m);
+	mj_deleteData(d);
+	
 
 	return 0;
 }
